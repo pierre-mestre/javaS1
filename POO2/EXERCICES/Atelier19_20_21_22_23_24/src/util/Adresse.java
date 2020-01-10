08cf9cddcb811ee25571f0fca5448e0cff7af123
mestrep <mestrep@pc5309u.lan.esiee.fr>
2020-01-10 11:26:27 +0100

    finish Atelier19

---
+++ POO2/EXERCICES/Atelier19_20_21_22_23_24/src/util/Adresse.java
@@ -0,0 +1,62 @@
+package util;
+
+public class Adresse {
+
+
+	String voie;
+	String complementAdresse;
+	String ville;
+	String codePostal;
+	
+	public Adresse() {
+		super();
+		this.voie = "chemin de la reussite";
+		this.complementAdresse = "serveurs de l'esiee PARIS";
+		this.ville = "Noisy le GRAND";
+		this.codePostal = "93160";
+	}
+	public Adresse(String voie, String complementAdresse, String ville, String codePostal) {
+		super();
+		this.voie = voie;
+		this.complementAdresse = complementAdresse;
+		this.ville = ville;
+		this.codePostal = codePostal;
+	}
+	@Override
+	public String toString() {
+		return "Adresse [voie=" + voie + ", complementAdresse=" + complementAdresse + ", ville=" + ville
+				+ ", codePostal=" + codePostal + "]";
+	}
+	public String getVoie() {
+		return voie;
+	}
+
+	public void setVoie(String voie) {
+		this.voie = voie;
+	}
+
+	public String getComplementAdresse() {
+		return complementAdresse;
+	}
+
+	public void setComplementAdresse(String complementAdresse) {
+		this.complementAdresse = complementAdresse;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public String getCodePostal() {
+		return codePostal;
+	}
+
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+		
+}
